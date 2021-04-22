2e689cffe1974f98e3bb30509656722ac756e801
Henry Lee <nynxzx@gmail.com>
2021-04-22 11:30:19 +1000

    Initial Placeholder Content View

--- FoodPlaces/ContentView.swift
+++ FoodPlaces/ContentView.swift
@@ -4,13 +4,17 @@
 //
 //  Created by Henry Lee on 22/4/21.
 //
+// This is the view our links will link to - it will contain the image, name, location, notes and reviews (list)
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List{
+            Image("rockandroll")
+                .resizable()
+                .scaledToFit()
+        }
     }
 }
 
