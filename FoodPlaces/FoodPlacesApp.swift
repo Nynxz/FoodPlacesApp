f5773c6e380a613037e4cc399b37122104447abd
Henry Lee <nynxzx@gmail.com>
2021-04-22 11:09:21 +1000

    Initial Commit

---
+++ FoodPlaces/FoodPlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPlacesApp.swift
+//  FoodPlaces
+//
+//  Created by Henry Lee on 22/4/21.
+//
+
+import SwiftUI
+
+@main
+struct FoodPlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
