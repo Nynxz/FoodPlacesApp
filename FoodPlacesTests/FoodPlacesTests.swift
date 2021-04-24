ddb09322b1c7a1574d664ca646bacb22ba614844
Henry Lee <nynxzx@gmail.com>
2021-04-24 21:15:37 +1000

    Add Simple Tests for Place and ViewModel

--- FoodPlacesTests/FoodPlacesTests.swift
+++ FoodPlacesTests/FoodPlacesTests.swift
@@ -9,25 +9,21 @@ import XCTest
 @testable import FoodPlaces
 
 class FoodPlacesTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    var simpleTestPlace = Place(name: "Test Place", image: "rockandroll")
+    
+    var placesViewModel = ViewModel()
+    
+    func testPlaceName() throws {
+        XCTAssertEqual(simpleTestPlace.name, "Test Place")
+    }
+    func testPlaceImageName() throws {
+        XCTAssertEqual(simpleTestPlace.image, "rockandroll")
+    }
+    
+    func testDefaultViewModel() throws {
+        XCTAssertEqual(placesViewModel.model[0], Place(name: "Rock and Roll", image: "rockandroll"))
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
