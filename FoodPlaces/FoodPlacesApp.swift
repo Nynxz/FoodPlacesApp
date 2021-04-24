f8b3040e83ec94cefc86d4ea001b2aec5a1b1721
Henry Lee <nynxzx@gmail.com>
2021-04-25 00:56:24 +1000

    Added Saving and Loading - From Lecture

--- FoodPlaces/FoodPlacesApp.swift
+++ FoodPlaces/FoodPlacesApp.swift
@@ -10,14 +10,46 @@ import SwiftUI
 @main
 struct FoodPlacesApp: App {
     
-    //Our App Title
     @State var appTitle = "Food Places"
-    //Our Main Data
-    @StateObject var placesViewModel = ViewModel()
-
+    @ObservedObject var placesViewModel: ViewModel = {
+        guard let data = try? Data(contentsOf: FoodPlacesApp.fileURL),
+              let model = try? JSONDecoder().decode(ViewModel.self, from: data) else {
+            print("Loading Defaults")
+            return ViewModel()
+        }
+        print("Loading Saved")
+        return model
+    }()
+    
     var body: some Scene {
         WindowGroup {
-            MasterView(title: $appTitle, placesVM: placesViewModel)
+            MasterView(
+                title: $appTitle,
+                placesVM: placesViewModel)
         }
     }
+    
+    static var fileURL: URL {
+        let fileName = "places.json"
+        let fm = FileManager.default
+        guard let documentDir = fm.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            return URL(fileURLWithPath: "/") }
+        let fileURL = documentDir.appendingPathComponent(fileName)
+        return fileURL
+    }
+    
+    static func save(viewModel: ViewModel) {
+        do {
+            let data = try JSONEncoder().encode(viewModel)
+            try data.write(to: fileURL, options: .atomic)
+            guard let dataString = String(data: data, encoding: .utf8) else {
+                return
+            }
+            print("Saved \(dataString)")
+        } catch{
+            print("Could not write file: \(error)")
+        }
+    }
+    
+    
 }
