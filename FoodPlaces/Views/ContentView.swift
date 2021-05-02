05fb921aa1d0f8abc5effac9f6b325755b5f5303
Henry Lee <nynxzx@gmail.com>
2021-05-02 18:01:47 +1000

    Add Content to Content View

--- FoodPlaces/Views/ContentView.swift
+++ FoodPlaces/Views/ContentView.swift
@@ -16,39 +16,113 @@ struct ContentView: View {
     var body: some View {
         List{
             if editMode?.wrappedValue.isEditing ?? true {
-                
+                Text("Title")
+                    .font(.title2)
+                    .bold()
                 TextField("Title", text: Binding(
-                    get: {
-                        return place.name
-                    },
-                    set: {
-                        (newValue) in
-                        place.name = newValue
-                    }),
-                    onCommit: {
-                        FoodPlacesApp.save(viewModel: viewModel)
-                    })
+                            get: {
+                                return place.name
+                            },
+                            set: {
+                                (newValue) in
+                                place.name = newValue
+                            }),
+                          onCommit: {
+                            FoodPlacesApp.save(viewModel: viewModel)
+                          })
+                
+                Text("Location")
+                    .font(.title2)
+                    .bold()
+                TextField("Location", text: Binding(
+                            get: {
+                                return place.location
+                            },
+                            set: {
+                                (newValue) in
+                                place.location = newValue
+                            }),
+                          onCommit: {
+                            FoodPlacesApp.save(viewModel: viewModel)
+                          })
             }
             else {
                 Text(place.name)
                     .font(.title)
                     .fontWeight(.heavy)
+                Text(place.location)
+                    .font(.subheadline)
+                    .fontWeight(.heavy)
             }
-
-            Image(place.image)
-                .resizable()
-                .scaledToFit()
-                .cornerRadius(16)
-
+            
+            if editMode?.wrappedValue.isEditing ?? true {
+                Text("Image URL")
+                    .font(.title2)
+                    .bold()
+                TextField("URL", text: Binding(
+                            get: {
+                                return place.imageURL
+                            },
+                            set: {
+                                (newValue) in
+                                place.imageURL = newValue
+                                place.rawImage = returnDataFromURL(url: place.imageURL)
+                            }),
+                          onCommit: {
+                            FoodPlacesApp.save(viewModel: viewModel)
+                          })
+            }
+            else {
+                returnImageFromData(data: place.rawImage)
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(16)
+            }
+            
+            Text("Information")
+                .font(.subheadline)
+                .fontWeight(.heavy)
+            TextEditor(text: Binding(
+                        get: {
+                            return place.information
+                        },
+                        set: {
+                            (newValue) in
+                            place.information = newValue
+                        }))
+                .frame(minHeight: 100)
+            
+            
+            VStack(alignment: .leading){
+                Button("Add"){
+                    place.addRecommendation()
+                }
+                Text("Recomendation")
+                    .font(.subheadline)
+                    .fontWeight(.heavy)
+                
+                List{
+                    ForEach(place.recommendations, id: \.self) { rec in
+                        Text(rec)
+                        //Had Issues Allowing Text to be editable
+                    }
+                    .onDelete(perform: { indexSet in
+                        place.recommendations.remove(atOffsets: indexSet)
+                        FoodPlacesApp.save(viewModel: viewModel)
+                        
+                    })
+                    .onMove(perform: { indices, newOffset in
+                        place.recommendations.move(fromOffsets: indices, toOffset: newOffset)
+                        FoodPlacesApp.save(viewModel: viewModel)
+                        
+                    })
+                }
+                .frame(minHeight: 300)
+                
+            }
+            .navigationBarItems(trailing: EditButton())
         }
-        .navigationBarItems(trailing: EditButton())
         
     }
 }
 
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//        MasterView()
-//    }
-//}
