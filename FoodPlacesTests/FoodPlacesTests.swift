5652336a634e5ecd79d822f2e7a231e961b7e5cc
Henry Lee <nynxzx@gmail.com>
2021-05-02 18:08:40 +1000

    Update Tests for New Place Model

--- FoodPlacesTests/FoodPlacesTests.swift
+++ FoodPlacesTests/FoodPlacesTests.swift
@@ -10,26 +10,47 @@ import XCTest
 
 class FoodPlacesTests: XCTestCase {
     
-    var simpleTestPlace = Place(name: "Test Place", image: "rockandroll")
+    var simpleTestPlace = Place(
+        name: "Rock and Roll 3",
+        image: "rockandroll",
+        location: "Brisbane",
+        imageURL: "https://cdn.usarestaurants.info/assets/uploads/e065b0011f15d5ff4c23aabfcfe8d4b2_-united-states-illinois-dupage-county-milton-township-carol-stream-95558-augustinos-rock-and-roll-deli-and-grillhtm.jpg",
+        rawImage: returnDataFromURL(url: "https://cdn.usarestaurants.info/assets/uploads/e065b0011f15d5ff4c23aabfcfe8d4b2_-united-states-illinois-dupage-county-milton-township-carol-stream-95558-augustinos-rock-and-roll-deli-and-grillhtm.jpg"),
+        information: "Default Info",
+        recommendations: ["Good Place", "Many Food"])
     
     var placesViewModel = ViewModel()
     
     func testPlaceName() throws {
-        XCTAssertEqual(simpleTestPlace.name, "Test Place")
+        XCTAssertEqual(simpleTestPlace.name, "Rock and Roll 3")
     }
     func testPlaceImageName() throws {
         XCTAssertEqual(simpleTestPlace.image, "rockandroll")
     }
     
     func testDefaultViewModel() throws {
-        XCTAssertEqual(placesViewModel.model[0], Place(name: "Rock and Roll 1", image: "rockandroll"))
+        XCTAssertEqual(placesViewModel.model[0], Place(
+                        name: "Rock and Roll 3",
+                        image: "rockandroll",
+                        location: "Brisbane",
+                        imageURL: "https://cdn.usarestaurants.info/assets/uploads/e065b0011f15d5ff4c23aabfcfe8d4b2_-united-states-illinois-dupage-county-milton-township-carol-stream-95558-augustinos-rock-and-roll-deli-and-grillhtm.jpg",
+                        rawImage: returnDataFromURL(url: "https://cdn.usarestaurants.info/assets/uploads/e065b0011f15d5ff4c23aabfcfe8d4b2_-united-states-illinois-dupage-county-milton-township-carol-stream-95558-augustinos-rock-and-roll-deli-and-grillhtm.jpg"),
+                        information: "Default Info",
+                        recommendations: ["Good Place", "Many Food"]))
     }
     
     func testViewModelAddElement() throws {
         let placesViewModelTemp = ViewModel()
         let currentLength = placesViewModelTemp.model.count
         placesViewModelTemp.addElement()
-        XCTAssertEqual(placesViewModelTemp.model[currentLength], Place(name: "\(currentLength+1) Place", image: "rockandroll"))
+        XCTAssertEqual(placesViewModelTemp.model[currentLength], Place(
+                        name: "\(currentLength+1) Place",
+                        image: "rockandroll",
+                        location: "Brisbane",
+                        imageURL: "https://cdn.usarestaurants.info/assets/uploads/e065b0011f15d5ff4c23aabfcfe8d4b2_-united-states-illinois-dupage-county-milton-township-carol-stream-95558-augustinos-rock-and-roll-deli-and-grillhtm.jpg",
+                        rawImage: returnDataFromURL(url: "https://cdn.usarestaurants.info/assets/uploads/e065b0011f15d5ff4c23aabfcfe8d4b2_-united-states-illinois-dupage-county-milton-township-carol-stream-95558-augustinos-rock-and-roll-deli-and-grillhtm.jpg"),
+                        information: "New Information",
+                        recommendations: ["Good Place", "Many Food"]))
     }
     
 
